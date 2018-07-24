6adffb0b35b82deae2193ec0d95cd9fe6093efa4
HABELFOC <habeljustin48@gmail.com>
2018-07-24 09:49:25 +0800

    'refactor-credentials'

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
 mongoose
-.connect(db)
+.connect(proccess.env.mongoURIVar)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
